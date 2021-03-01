cbf09c9316dda791827f57e48fb4d88515a07c29
cajunintexas.cm@gmail.com <cajunintexas.cm@gmail.com>
2021-03-01 15:55:33 -0600

    api created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { request } = require('express');
 
 const app = express();
 app.use(bodyParser.json());
@@ -26,15 +27,97 @@ const todoList = [
   },
 ];
 
+let nextId = 5;
+
 // GET /api/todos
+app.get('/api/todos', (req, res) => {
+  res.json(todoList);  /* sends back todoList array as JSON file */
+})
 
 // GET /api/todos/:id
+app.get('/api/todos/:id', (req, res) => {
+  // get id from route
+  const id = Number(req.params.id);
+  // const id = parseInt(req.params.id); /* another way to get all info */
+  // const id = +req.params.id;  /* another way to get all info */
+  // use id to find one todo item
+  const todo = todoList.find((currTodo) => {
+    if (currTodo.id == id) {
+      return true
+    } else {
+      return false
+    }
+  });
+
+  if (!todo) {
+    res.status(404).json({
+      error: `Could not find todo with ID:  ${id}`
+    })    
+  } else {
+  //  send back todo
+  res.json(todo)
+  }
+});
 
 // POST /api/todos
+app.post('/api/todos', (req, res) => {
+  if (req.body.description) {
+  const newTodo = {
+    id: nextId++,
+    description: req.body.description
+  }
+  todoList.push(newTodo);
+  res.status(201);
+  res.send();
+  } else {
+    res.status(422)
+    res.json({
+      error: 'please add a description'
+    })
+  }
+});
 
 // PUT /api/todos/:id
+app.patch('/api/todos/:id', (req, res) => {
+  // if req.body contains a description
+  if (req.body.description || req.body.description === '') {
+    //get id from route  
+  const id = Number(req.params.id);
+  // find where the todo exists in the todoList array
+  const todoIndex = todoList.findIndex((currTodo) => currTodo.id === id ? true : false)        
+  // update the object inside of the the todoList array
+  todoList[todoIndex].description = req.body.description
+  // send back the updated todo item  
+  res.json(todoList[todoIndex])
+  } else {
+    res.status(422).json({
+      error: 'Please provide a description'
+    })
+  }
+});
+
 
 // DELETE /api/todos/:id
+app.delete('/api/todos/:id', (req, res) => {
+  // find which oue to delete
+  const id = Number(req.params.id);
+  // find where the todo exists in the todoList array
+  const todoIndex = todoList.findIndex((currTodo) => currTodo.id === id) 
+  if(todoIndex !== -1) {  
+  // remove it from array
+  todoList.splice(todoIndex, 1)
+  // respond with appropriate status/response
+  res.status(204).json()
+  } else {
+    res.status(404).json({ error: `Could not find todo with id: ${id}`})
+  }
+});
+
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log('Todo List API is now listening on port 3000...');
